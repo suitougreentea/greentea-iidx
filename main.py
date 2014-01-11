f6b991aef902d70753198ffd1db1f49cdbff98f4
suitougreentea <suitougreentea@users.noreply.github.com>
2014-01-11 17:13:05 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from google.appengine.api import users
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.write('Hello, world!')
+
+app = webapp2.WSGIApplication([
+	('//', MainHandler)
+	], debug=True)
